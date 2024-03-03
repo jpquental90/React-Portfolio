0d47d4c23da75a3666b065e7c81b48ac7b1fcf6c
Selene <149156667+joanapq@users.noreply.github.com>
2024-03-03 20:16:07 +0000

    added profile pic to home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import './home.css'
 
+// This should be a welcome landing page that contains:
+
+// Your Name
+
+// A headshot of you (or an avatar)
+
+// Your brand statement
+
+// Some indication that this is your portfolio site
+
 function Home() {
   return (
     <div className="home-wrapper">
       <div className="card" style={{width: '18rem'}}>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">Front-End Developer</h5>
           <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#" className="card-link">Card link</a>
           <a href="#" className="card-link">Another link</a>
         </div>
       </div>
-      <div className="card" style={{width: '18rem'}}>
+      <div className="card card-picture" style={{width: '18rem'}}>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="card-link">Card link</a>
-          <a href="#" className="card-link">Another link</a>
+          <img src="images/profilepic.jpg" alt="profile picture" className="profile-pic"/>
         </div>
       </div>
     </div>

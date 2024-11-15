2a6f3a0efe47269d8d8fa805ee3c926741151bbf
Joana Quental <joanapquental@gmail.com>
2024-11-15 12:11:54 +0000

    updated navbar to enable collapsed menu in smaller screen sizes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import './header.css'
+import './header.css';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+    document.querySelector('.navbar-collapse').classList.remove('show');
+  };
+
   return (
-    <div className="navbar-wrapper d-flex align-items-center justify-content-between">
-      <div className="logo-container d-flex align-items-center">
-        <img src="images/JQlogo.png" alt="name logo" className="logo-pic" />
-        <h3 className="name-title">Joana Quental</h3>
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid">
+        {/* Logo */}
+        <div className="logo-container d-flex align-items-center">
+          <img src="images/JQlogo.png" alt="name logo" className="logo-pic" />
+          <h3 className="name-title">Joana Quental</h3>
+        </div>
+
+        {/* Toggler Button with Dynamic Icon */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleMenu}
+          aria-controls="navbarNav"
+          aria-expanded={menuOpen}
+          aria-label="Toggle navigation"
+        >
+          {menuOpen ? '✖' : <span className="navbar-toggler-icon"></span>}
+        </button>
+
+        {/* Collapsible Menu */}
+        <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                onClick={closeMenu}
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/about"
+                onClick={closeMenu}
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="nav navbar d-flex">
-        <li className="nav-item">
-          <NavLink
-            to="/"
-            className="nav-link"
-            activeClassName="active-link"
-          >
-            Projects
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/about"
-            end
-            className="nav-link"
-            activeClassName="active-link"
-          >
-            About
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    </nav>
   );
 }
 
